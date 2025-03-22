c7b010615206579bcb0b69de9a87127ca2b73b7e
Yash <yashxerox15@gmail.com>
2025-03-23 01:21:24 +0530

    add theme,base, utilities tailwindcss in globals.css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <>
-      <h1 className="heading">Home</h1>
+      <section className="pink_container pattern">
+        <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
+        <p className="sub_heading !max-w-3xl text-white bg-[#122a4f]/ py-1 px-4">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+        </p>
+      </section>
     </>
   );
 }
