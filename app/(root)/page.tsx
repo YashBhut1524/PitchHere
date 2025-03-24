acc053e38edf9ed7e04e4bec62ae261c0bad573a
Yash <yashxerox15@gmail.com>
2025-03-24 18:36:14 +0530

    add query to fetch posts and setup types using sanity typegen

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,26 @@
-import StartupCard from "../../components/StartupCard";
+import StartupCard, { StartupTypeCard } from "../../components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { client } from "../../sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
 
   const query = (await searchParams).query
 
-  const post = [
-    { 
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 1, name: "Yash"},
-      _id: 1,
-      description: "This is the description",
-      image: "https://allthings.how/content/images/wordpress/2022/02/allthings.how-what-does-idk-mean-and-how-to-use-it-untitled-design.png",
-      category: "Robots",
-      title: "We Robots",
-    },
-]
+  const posts = await client.fetch(STARTUPS_QUERY)
+  console.log(JSON.stringify(posts, null, 2));
+//   const post = [
+//     { 
+//       _createdAt: new Date(),
+//       views: 55,
+//       author: { _id: 1, name: "Yash"},
+//       _id: 1,
+//       description: "This is the description",
+//       image: "https://allthings.how/content/images/wordpress/2022/02/allthings.how-what-does-idk-mean-and-how-to-use-it-untitled-design.png",
+//       category: "Robots",
+//       title: "We Robots",
+//     },
+// ]
 
   return (
     <>
@@ -34,8 +38,8 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
           {query ? `Search results for ${query}` : "All Startups"}
         </p>
         <ul className="mt-7 card_grid">
-          {post.length > 0 ? (
-            post.map((post: StartupCardType) => (
+          {posts.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post}/>
             ))
           ) : (
