9b941de6bd75be66f4bb7d1cdcfd56ec1b21fe6b
Yash <yashxerox15@gmail.com>
2025-03-24 21:37:27 +0530

    add realtime all startups render and add search query

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,17 @@
 import StartupCard, { StartupTypeCard } from "../../components/StartupCard";
 import SearchForm from "../../components/SearchForm";
-import { client } from "../../sanity/lib/client";
+// import { client } from "../../sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
 
   const query = (await searchParams).query
+  const params = {search: query || null}
 
-  const posts = await client.fetch(STARTUPS_QUERY)
-  console.log(JSON.stringify(posts, null, 2));
-//   const post = [
-//     { 
-//       _createdAt: new Date(),
-//       views: 55,
-//       author: { _id: 1, name: "Yash"},
-//       _id: 1,
-//       description: "This is the description",
-//       image: "https://allthings.how/content/images/wordpress/2022/02/allthings.how-what-does-idk-mean-and-how-to-use-it-untitled-design.png",
-//       category: "Robots",
-//       title: "We Robots",
-//     },
-// ]
+  // const posts = await client.fetch(STARTUPS_QUERY)
+  // console.log(JSON.stringify(posts, null, 2));
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params})
 
   return (
     <>
@@ -38,7 +29,7 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
           {query ? `Search results for ${query}` : "All Startups"}
         </p>
         <ul className="mt-7 card_grid">
-          {posts.length > 0 ? (
+          {posts?.length > 0 ? (
             posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post}/>
             ))
@@ -47,6 +38,7 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
